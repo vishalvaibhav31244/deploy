19204a7096ca6be639d5808f72d59c609754747c
vishalvaibhav31244 <76677092+vishalvaibhav31244@users.noreply.github.com>
2021-01-20 12:29:05 +0530

    Update buid.py

--- buid.py
+++ buid.py
@@ -1,4 +1,5 @@
-a=int(input("enter the 1st number :"))
-b=int(input("enter the 2nd number:"))
+import random
+a=random.randint(0,9)
+b=random.randint(0,9)
 c=a+b
 print("sum of two number a and b is: ",c)
